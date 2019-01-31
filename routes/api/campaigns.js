8d716b54459e3f7c4500c8bf2a6b8897c2639adc
jrmoore117 <jrmoore117@gmail.com>
2019-01-30 16:34:24 -0800

    Add temporary methods to campaignsController.js.

--- routes/api/campaigns.js
+++ routes/api/campaigns.js
@@ -3,12 +3,12 @@ const campaignsController = require("../../controllers/campaignsController");
 
 // Matches with "/api/campaigns"
 router.route("/")
-  .get(campaignsController.findAll)
+//   .get(campaignsController.findAll) - May not use this because we don't want to find ALL campaigns. We want all campaigns for logged in user (Like on line 11).
   .post(campaignsController.create);
 
 // Matches with "/api/campaigns/:userName"
 router.route("/:userName")
-  .get(campaignsController.findById)
+  .get(campaignsController.findByUsername)
   .put(campaignsController.update)
   .delete(campaignsController.remove);
 
