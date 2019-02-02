4698a2ecb00dd058b5c2e1962fd661935e850390
ZCDEV <zcdev@Orange.attlocal.net>
2019-02-01 22:28:38 -0800

    Rewrite code

--- client/src/components/EncounterItem/index.js
+++ client/src/components/EncounterItem/index.js
@@ -2,20 +2,24 @@ import React from "react";
 import "./style.css";
 
 function EncounterItem(props) {
-    return (
-        <div className="card" onClick={()=> props.handleDisplay(props.id)}>
-            <div className="content">
-            <ul>
-                <li>
-                {props.name}
-                </li>
-            </ul>
-            </div>
-            <div className="img-container">
-            <img alt={props.name} src={props.image} />
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className="card"
+      onClick={() => props.handleDisplay && props.handleDisplay(props.id)}
+    >
+      <div className="content">
+        <ul>
+          <li>
+            {props.name}
+            {console.log(props)}
+          </li>
+        </ul>
+      </div>
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+    </div>
+  );
 }
-  
-  export default EncounterItem;
+
+export default EncounterItem;
--- client/src/components/SetupEncounters/index.js
+++ client/src/components/SetupEncounters/index.js
@@ -1,79 +1,46 @@
 import React, { Component } from "react";
 import EncounterItem from "../EncounterItem";
-import EncounterDisplay from "../EncounterDisplay";
-import EncounterNew from "../EncounterNew";
+import encounters from "../../EncountersTempData";
 
-// // Div
-//    // Div
-//       // EncounterItem components w/ onClick to display (1.)
-//       // button with onClick event to display (2.)
-//    // Div 
-//       // 1.) EncounterDisplay 
-// // 2.) EncounterNew component
-const encounters = [{id: 1, name: "monster1", image: "😂"}, {id: 2, name: "monster2", image: "😂"}];
 class SetupEncounters extends Component {
-    
-    state = {
-        displayItem: false,
-        newItem: false,
-        isEncountered: false,
-        encounters
-    };
+  state = {
+    encounters,
+    displayItem: null
+  };
 
-    handleDisplay(id) {
-        this.setState({
-            displayItem: true
-        });
-    }
+  handleDisplay = id => {
+    this.setState({
+      displayItem: encounters.find(encounter => encounter.id === id)
+    });
+  };
 
-    handleNew() {
-        this.setState({
-            newItem: true
-        });
-    }
-
-    handleDisplay = this.handleDisplay.bind(this);
-    handleNew = this.handleNew.bind(this);
-
-    render() {
-        if (this.state.encounters.length) {
-            this.state.isEncountered = true;
-        }
-        return (
-            <div>
-                <div id="cta">
-                    {this.state.isEncountered ? (
-                        <EncounterDisplay handleDisplay={this.handleDisplay} />
-                    ) : (
-                        <div>Please start with a new encounter</div>
-                    )}
-                    <button onClick={this.handleNew}>New</button>
-                </div>
-                <div id="render">
-                    {this.state.displayItem && (
-                    <div>
-                        <EncounterItem />
-                    </div>
-                    )}
-                    {this.state.newItem && (
-                    <div>
-                        <EncounterNew />
-                    </div>
-                    )}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div id="cta">
+          {this.state.encounters.map(encounter => (
+            <EncounterItem
+              id={encounter.id}
+              key={encounter.id}
+              name={encounter.name}
+              image={encounter.image}
+              handleDisplay={this.handleDisplay}
+            />
+          ))}
+        </div>
+        <div id="render">
+          {this.state.displayItem && (
+            <EncounterItem
+              id={this.state.displayItem.id}
+              key={this.state.displayItem.id}
+              name={this.state.displayItem.name}
+              image={this.state.displayItem.image}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
-
-//// The Dungeon Master Set-up
-// import React, { Component } from "react";
-// import API from "../../utils/API"
-// import { Link } from "react-router-dom";
-// import "./encounter.css"
-
-// // What we need from the Dungeon master Encounter setup
-// //  - Chosen monster list/stats API
-// //  the ability to create an encounter and then store it for later use
 
 export default SetupEncounters;
