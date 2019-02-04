c91258f004608fe3dee576965edf827fd9e32b87
jrmoore117 <jrmoore117@gmail.com>
2019-02-04 12:39:39 -0800

    Finish SetupEncounters page: When modal form is submitted, new encounter is created in the database.

--- client/src/components/SetupEncounters/index.js
+++ client/src/components/SetupEncounters/index.js
@@ -5,6 +5,7 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import EncounterMonsterList from "../SetupEncounters/EncounterMonsterList";
 import SelectedMonster from "../SetupEncounters/SelectedMonster";
+import EncounterDisplay from "../EncounterDisplay";
 import monsters from "../../dnd-data/monsters.json";
 import API from "../../utils/API";
 
@@ -12,9 +13,8 @@ class SetupEncounters extends Component {
   state = {
     encounters: [],
     newEncounter: [],
-    displayItem: null,
+    displayItem: {},
     show: false,
-    newItem: true,
     encounterName: ""
   };
 
@@ -26,12 +26,18 @@ class SetupEncounters extends Component {
           encounters: res.data
         })
       })
-};
+  };
 
   handleDisplay = id => {
+
+    // console.log(id);
+    // console.log("ENCOUNTERS: ", this.state.encounters);
+
     this.setState({
-      displayItem: this.state.encounters.find(encounter => encounter.id === id)
+      displayItem: this.state.encounters.find(encounter => encounter._id === id)
     });
+
+    // console.log(this.state.displayItem);
   };
 
   handleClose = () => {
@@ -51,9 +57,6 @@ class SetupEncounters extends Component {
 
   handleFormSubmit = event => {
     event.preventDefault();
-    // this.setState({
-    //   encounterName: ""
-    // });
 
     const newEncounter = {
       name: this.state.encounterName,
@@ -64,12 +67,12 @@ class SetupEncounters extends Component {
       .then(res => {
         // console.log(res)
         API.getEncountersFromCampaign(this.props.campaignId)
-        .then(res => {
-          // console.log(res)
-          this.setState({
-            encounters: res.data
+          .then(res => {
+            // console.log(res)
+            this.setState({
+              encounters: res.data
+            })
           })
-        })
       })
       .catch(err => console.log(err));
   };
@@ -90,71 +93,65 @@ class SetupEncounters extends Component {
     return (
       <div>
         <div id="cta">
-          {this.state.encounters.map(encounter => (
+          {this.state.encounters.map( (encounter, i) => (
             <EncounterItem
-              id={encounter.id}
-              key={encounter.id}
+              id={encounter._id}
+              // key={encounter.id}
               name={encounter.name}
               image={encounter.image}
               handleDisplay={this.handleDisplay}
+              key={i}
             />
           ))}
           <Button onClick={this.handleShow}>New</Button>
         </div>
         <div id="render">
-          {this.state.displayItem && (
-            <EncounterItem
-              id={this.state.displayItem.id}
-              key={this.state.displayItem.id}
-              name={this.state.displayItem.name}
-              image={this.state.displayItem.image}
-            />
-          )}
-          {this.state.newItem && (
-            <div>
-              <Modal show={this.state.show} onHide={this.handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title>New Encounter</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  <div>
-                    <form className="form">
-                      <input
-                        value={this.state.firstName}
-                        name="encounterName"
-                        onChange={this.handleInputChange}
-                        type="text"
-                        placeholder="Encounter Name"
-                      />
-                      <button onClick={this.handleFormSubmit}>Submit</button>
-                    </form>
-                    <div className="row">
-                      <div className="col-6" id="monster-list">
-                        <EncounterMonsterList addMonsterToEncounter={this.addMonsterToEncounter}/>
-                      </div>
-                      <div className="col-6" id="selected-monsters">
-                        Encounter Monsters
-                        {this.state.newEncounter.map( (monster, i) => (
-                          <SelectedMonster
-                            monster={monster}
-                            key={i}
-                          />
-                        ))}
-                      </div>
+          <EncounterDisplay
+            encounter={this.state.displayItem}
+          />
+          <div>
+            <Modal show={this.state.show} onHide={this.handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>New Encounter</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div>
+                  <form className="form">
+                    <input
+                      value={this.state.firstName}
+                      name="encounterName"
+                      onChange={this.handleInputChange}
+                      type="text"
+                      placeholder="Encounter Name"
+                    />
+                    <button onClick={this.handleFormSubmit}>Submit</button>
+                  </form>
+                  <div className="row">
+                    <div className="col-6" id="monster-list">
+                      <EncounterMonsterList addMonsterToEncounter={this.addMonsterToEncounter} />
+                    </div>
+                    <div className="col-6" id="selected-monsters">
+                      Encounter Monsters
+                        {this.state.newEncounter.map((monster, i) => (
+                        <SelectedMonster
+                          monster={monster}
+                          key={i}
+                        />
+                      ))}
                     </div>
                   </div>
-                </Modal.Body>
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={this.handleClose}>
-                    Close
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={this.handleClose}>
+                  Close
                   </Button>
-                  <Button variant="primary" onClick={this.handleClose}>
-                    Save
+                <Button variant="primary" onClick={this.handleClose}>
+                  Save
                   </Button>
-                </Modal.Footer>
-              </Modal>
-            </div>
-          )}
+              </Modal.Footer>
+            </Modal>
+          </div>
         </div>
       </div>
     );
--- client/src/pages/Setup/index.js
+++ client/src/pages/Setup/index.js
@@ -9,17 +9,24 @@ class Setup extends Component {
    render() {
       return (
          // All wrapped in router (only needed if SetupPlayers component is added to #setup-main div)
+
+         // <Router>
          <div id="setup">
             <div id="setup-sidebar">
                Setup Sidebar!
+               {/* <Link to="/setupEncounters" />*/}
+               {/* <Link to="/setupCharacters" />*/}
                {/* Has two nav buttons for Encounters and Players */}
             </div>
             <div id="setup-main">
                Setup Main!
                {/* Has two routes for SetupEncounters component*/}
+               {/* <Route component ={() => <SetupEncounters campaignId={this.props.campaignId}/>} */}
                <SetupEncounters campaignId={this.props.campaignId} />
+               {/* <Route component ={() => <SetupCharacters campaignId={this.props.campaignId}/>} */}
             </div>
          </div>
+         // </Router>
       );
    }
 }
