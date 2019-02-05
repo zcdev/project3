18b9b7e3bf137d84acce4b0f2d03e47f644a6265
Fermenting <realferalxanderyell@gmail.com>
2019-02-05 11:56:15 -0800

    refactoring monster display

--- client/src/pages/Encyclopedia/Monsters/MonsterDisplay.js
+++ client/src/pages/Encyclopedia/Monsters/MonsterDisplay.js
@@ -12,41 +12,51 @@ function MonsterDisplay(props) {
   if (props.monster) {
     return (
       <div>
-        <h3>{props.monster.name}</h3>
-        <p><strong>Actions</strong></p>
-        {props.monster.actions.map(action => (
-          <div>
-            <hr></hr>
-            <p>Name: {action.name}</p>
-            <p>Description: {action.desc}</p>
-          </div>
-        ))}
+        {displayActions()}
         {displaySpecialAbilities()}
       </div>
     );
 
   }
 
-  function displaySpecialAbilities() {
-    if (props.monster.special_abilities) {
+  function displayActions() {
+    if (props.monster.actions) {
       return (
         <div>
-          <h3>Special Abilities</h3>
-          {props.monster.special_abilities.map(ability => (
+          <h3>{props.monster.name}</h3>
+          <p><strong>Actions</strong></p>
+          {props.monster.actions.map(action => (
             <div>
               <hr></hr>
-                <p><strong>Name: </strong>{ability.name}</p>
-                <p><strong>Description: </strong>{ability.desc}</p>
-
+              <p>Name: {action.name}</p>
+              <p>Description: {action.desc}</p>
             </div>
           ))}
         </div>
-      );
+      )
     }
   }
 
-}
+    function displaySpecialAbilities() {
+      if (props.monster.special_abilities) {
+        return (
+          <div>
+            <h3>Special Abilities</h3>
+            {props.monster.special_abilities.map(ability => (
+              <div>
+                <hr></hr>
+                <p><strong>Name: </strong>{ability.name}</p>
+                <p><strong>Description: </strong>{ability.desc}</p>
+
+              </div>
+            ))}
+          </div>
+        );
+      }
+    }
+
+  }
 
 
 
-export default MonsterDisplay;
+  export default MonsterDisplay;
