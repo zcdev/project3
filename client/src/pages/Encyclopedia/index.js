66ed7ec51d01455d1b00aa6dc0ec2a7e9e0425ad
Fermenting <realferalxanderyell@gmail.com>
2019-02-04 23:03:39 -0800

    encyclopedia search starting to take shape

--- client/src/pages/Encyclopedia/index.js
+++ client/src/pages/Encyclopedia/index.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import MonsterList from "./Monsters/MonsterList";
+import MonsterDisplay from "./Monsters/MonsterDisplay";
+
+import monsters from "../../dnd-data/monsters.json";
 import "./style.css";
 
 class Encyclopedia extends Component {
@@ -6,16 +11,41 @@ class Encyclopedia extends Component {
 
    }
 
+   componentDidMount() {
+
+
+   }
+
+
+
    render() {
       return (
-         <div id="encyclopedia">
-            <div id="encyclopedia-sidebar">
-               Encyclopedia Sidebar!
+         <Router>
+            <div id="encyclopedia">
+               <div id="encyclopedia-sidebar">
+                  <div id="encyclopdia-nav">
+                     <div className="encyclopedia-nav-btn light">
+                        <Link to="/monsters">Monsters</Link>
+                     </div>
+
+                  </div>
+
+                  <div id="encyclopedia-main"> 
+                                  
+                     <div id="info-display">
+                     <Route
+                        path="/monsters"
+                        component={() => <MonsterList printMonsterStats={this.printMonsterStats} />}
+                     />
+                     </div>
+                     
+                     <div id="encyclopedia-display">
+                     <MonsterDisplay monster={this.state.currentMonster} />
+                     </div>
+                  </div>
+               </div>
             </div>
-            <div id="encyclopedia-main">
-               Encyclopedia Main!
-            </div>
-         </div>
+         </Router>
       );
    }
 }
